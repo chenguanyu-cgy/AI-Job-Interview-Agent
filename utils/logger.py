38f6b4d8d63cba4b30c4f73f58bcc997667a802b
chenguanyu <1316812215@qq.com>
2026-05-08 23:36:18 +0800

    fix: 修复logger重复定义问题

--- utils/logger.py
+++ utils/logger.py
@@ -1,10 +1,20 @@
 import os
+import logging
 from config import Config
-from loguru import logger
 
+# 确保日志文件夹存在
 if not os.path.exists(Config.LOGS_FOLDER):
     os.makedirs(Config.LOGS_FOLDER)
-    logger.add(
-        os.path.join(Config.LOGS_FOLDER, "logs.log"),
-        rotation="10 MB",
-        encoding="utf-8",)
+
+# 配置日志格式
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler(os.path.join(Config.LOGS_FOLDER, "logs.log"), encoding="utf-8"),
+        logging.StreamHandler()
+    ]
+)
+
+# 定义logger对象
+logger = logging.getLogger(__name__)
